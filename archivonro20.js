bd7c97c524beee3e865184c85f0692cddff5cc33
andresraul <andresraulvaras@yahoo.com>
2021-07-11 15:31:31 -0300

    modified files

--- archivonro20.js
+++ archivonro20.js
@@ -2,3 +2,4 @@ archivo de prueba
 archivo de prueba
 archivo de prueba
 archivo de prueba 
+Hoy es domingo 11 de julio del año 2021
---
+++ archivonuevo.js
@@ -0,0 +1,49 @@
+Homework
+
+En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se los 
+estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rapidamente cualquier agujero en tu 
+comprensión.
+
+Variables
+
+En computación se usa la palablra variable usando sus tres primeras letras VAR para saber rapidamente su significado
+así cuando vemos las letras VAR en minúscula nos damos cuenta que es una variable. Esta variable nos permite o podemos
+almacenar o guardar un valor o una palabra que podemos usar en otra tarea o indicación para hacer algo más . 
+
+Strings
+
+Las strings podemos identificarlas porque son un conjunto de caracteres, podemos decir también que es una cadena de 
+caracteres pero tiene un principio y un fin y nos podemos dar cuenta porque siempre están encerradas entre comillas 
+Una strings comienza con comillas y luego sigue un conjunto o cadena de caracteres y al final termina con comillas. 
+Hay que recordar siempre que empiezan con comillas y terminan con comillas. 
+
+Funciones
+
+Una función en computación podemos entender que actúa como un procedimiento que puede tener una o mas tareas o 
+instrucciones para que realice un trabajo o obtenga un valor 
+
+Argumentos
+
+Los argumentos van encerrados entre paréntesis y se pueden agregar varios argumentos siempre seguido de coma por 
+ejemplo podemos poner entre paréntesis nombres, números etcétera. 
+
+Return
+
+La palabra Return significa retorno y se usa esta pálabra para indicarle a la función que nos retorne un valor 
+podemos ver que tiene el trabajo de indicarle a la función que informe o diga o nos de un valor que ha procesado 
+la función. 
+
+Declaraciones IF
+
+La palabra IF tiene la función de comparar un valor o palabra para seguir trabajando o parar en caso de que no sea 
+el caso. Podemos ver el caso práctico de una persona que controla la entrada a una sala, esta persona tiene la función 
+de IF si el visitante tiene la entrada puede ingresar y si el visitante no tiene la entrada el visitante no puede 
+ingresar 
+
+Valores Booleanos TRUE FALSE
+
+Los valores booleanos que traducidos significan verdadero o falso significa que tiene solo dos opciones verdadero o 
+falso como las palabras lo indican
+
+
+
