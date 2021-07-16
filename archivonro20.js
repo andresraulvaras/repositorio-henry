be46881f4afe7fff3b5831e49c2d2f8fcf5af52e
andresraul <andresraulvaras@yahoo.com>
2021-07-16 14:18:25 -0300

    date

--- archivonro20.js
+++ archivonro20.js
@@ -4,3 +4,7 @@ archivo de prueba
 archivo de prueba 
 Hoy es domingo 11 de julio del año 2021
 Hoy es lunes 12 de julio del año 2021
+Hoy es martes 13 de julio del año 2021
+Hoy es miercoles 14 de julio del año 2021
+Hoy es jueves 15 de julio del año 2021
+Hoy es viernes 16 de julio del año 2021
