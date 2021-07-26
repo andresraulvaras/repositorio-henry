e0c96ebc6ea3f5d4524d1bcaf61ac7894168399d
andresraul <andresraulvaras@yahoo.com>
2021-07-26 20:24:01 -0300

    Tarea

---
+++ archivo03jsII.js
@@ -0,0 +1,44 @@
+Homework 03-JS-II
+
+En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se
+los estuvieras explicando a un niño de 12 años.
+
+for : con estas tres letras podemos identificar que el programa va a realizar un proceso que se va a repetir,
+para que suceda la repetición for va a necesitar de la ayuda de tres ayudas como si fueran empleados que van
+a realizar una parte del trabajo, el primer ayudante es el que va a dar nombre a la acción de repetir el
+proceso, por ejemplo lo vamos a llamar letra A en minúscula que va a ser igual a cero, teniendo identificado
+A en cero, el segundo ayudante va a indicarle al proceso que A debe llegar hasta determinada acción y ahí se 
+detendrá y no va a continuar incrementando su valor, el tercer ayudante tiene la funcion de incrementar el 
+valor de A de uno en uno, y así ya se tiene el proceso de for . Ahora podemos decir que en for el proceso es
+primero asignar una letra con un valor inicial, segundo la letra va a tener un valor límite hasta donde tiene
+que llegar y tercero la letra se va a incrementar de uno en uno.
+
+&& 
+
+Estos dos símbolos juntos se ocupan en computación para hacer una comparación, funciona como nuestro Y en 
+castellano, su función es parecido al siguiene ejemplo, teniendo un valor lo comparamos con dos condiciones
+si un valor A no es mayor a un valor B, aquí colocamos el Y del castellano && el valor A no es menor que C 
+entonces si las dos comparaciones son verdaderas el proceso de la computadora va a dar un valor verdadero
+si una comparación no es verdadera el proceso de la computadora va a dar un valor falso.
+
+||
+
+Estos dos simbolos juntos se ocupan en computación para hacer otra comparación, funciona como nuestro O en 
+castellano, su función es parecido al siguiente ejemplo, teninendo un valor A lo comparamos con dos condiciones
+si un valor A no es mayor a un valor B, aquí colocamos el O del castellano || el valor A no es menor que C 
+entonces si UNA comparación es verdadera el proceso de la computadora va a dar un valor verdadero, si las dos 
+comparaciones son falsas, entonces el proceso de la computadora va a dar un valor falso.
+
+! 
+
+Con este símbolo, el signo de exclamación, se ocupa en computación para indicar el valor opuesto a la 
+comparación que estemos realizando, por ejemplo, si se le coloca este simbolo al valor falso (!falso),
+el proceso de la computadora va a dar un resultado que indica lo opuesto a falso, es decir, NO FALSO,
+y si se le coloca el simbolo a verdadero (!verdadero), el proceso de la computadora va a dar un resultado 
+que indica lo opuesto a verdadero, es decir NO VERDADERO 
+
+
+Fecha: Hoy es lunes 26 de julio del año 2021 
+Andrés Raúl Varas 
+
+
