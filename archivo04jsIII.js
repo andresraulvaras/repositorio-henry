53ab69913c7850105706c160dd0155a1b0eb853a
andresraul <andresraulvaras@yahoo.com>
2021-08-17 16:26:14 -0300

    homework

---
+++ archivo04jsIII.js
@@ -0,0 +1,25 @@
+Homework javascript III
+
+En un archivo de texto separado que debes crear, escrible explicaciones de los siguientes conceptos como si se
+lo estuvieras explicando a un niño de 12 años. 
+
+Tema: Arrays 
+
+Los arrays en javascript se pueden considerar o comprender dentro de los programas de las computadoras como una
+lista de items como si fuera la lista de estudiantes de un curso de alumnos, como si fuese una lista de productos
+de alimentos que debemos comprar en un almacén, como una lista de nuestros mubles que tenemos en la casa, también
+como una lista de los años que tienen cada uno de nuestros familiares. Es decir que en computación se pueden crear 
+listas con valores que consideramos importantes para hacer una tarea o actividad, éstas listas reciben un nombre 
+y se encierran entre corchetes, y al igual que nosotros podemos modificar la lista en computación se pueden 
+modificar esas listas con determinadas palabras, vamos a poder agregar o quitar items de las listas, también 
+vamos a poder ver que item figura en determinada posición por ejemplo el cuarto lugar o el último lugar .
+
+Resumiendo los arrays son el lugar donde se puede almacenar información por ejemplos nombres, números, o cualquier
+otra información, estos arrays tienen un nombre que nosotros podemos definir y siempre se encierran entre
+corchetes y van separados por una coma.
+
+
+Hoy es martes 17 de agosto del año 2021
+Andrés Raúl Varas
+
+
